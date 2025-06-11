d15ab52557264d7192f182d4ddcca2603c800909
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-06-11 11:24:45 +0200

    * Adding register function * Adding login function with token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Dotenv\Exception\ValidationException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(RegisterRequest $request)
     {
+
+           $user = User::create([
+                "name" => $request->get("name"),
+                "email" => $request->get("email"),
+                "password" => Hash::make($request->get("password")),
+            ]);
+
+            return response()->json([
+                "status" => true,
+                "message" => "Registered successfully",
+                "data" => [
+                    "id" => $user->id,
+                    "name" => $user->name,
+                    "email" => $user->email
+                ]
+            ], 201);
+
+
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only("email", "password");
+
+        if(!Auth::attempt($credentials))
+        {
+            return response()->json([
+                'status' => false,
+                'message' => "Invalid credentials"
+            ], 401);
+        }
+        $user = Auth::user();
+        $token = $user->createToken('Api Token')->plainTextToken;
+
+        return response()->json([
+            "status" => true,
+            "message" => "Login successfully",
+            "data" => [
+                "id" => $user->id,
+                "email" => $user->email,
+            ],
+            "token" => $token
+        ], 200);
 
     }
 }
