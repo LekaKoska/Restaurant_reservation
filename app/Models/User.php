95186bea2221a3f571e74f101031e04127d11eea
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-07-01 13:52:19 +0200

    * initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function reservedTable()
+    {
+        return $this->hasOne(TablesInfoListModel::class);
+    }
 }
