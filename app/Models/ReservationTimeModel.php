e2e1440e3ed0f63d8494f3bb30e8c7faaafcb283
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-07-11 09:05:09 +0200

    * Creating model for reservation table * adding relation reservationTime

---
+++ app/Models/ReservationTimeModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReservationTimeModel extends Model
+{
+    protected $table = "reservation";
+    protected $fillable = ['user_id', 'table_id', 'reservation_date'];
+
+    protected $hidden = ['id', 'created_at', 'updated_at'];
+
+    public function   reservetionTime()
+    {
+        return $this->hasOne(TablesModel::class);
+    }
+
+}
