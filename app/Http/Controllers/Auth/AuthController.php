0d261782e68e2131cf2c10550b2ab1b546f45c9a
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-07-28 13:16:22 +0200

    * Creating Swagger @OA for Auth endpoints, * Register, Login, Logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -11,11 +11,51 @@ use App\Repository\AuthRepository;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use OpenApi\Annotations as OA;
 
 class AuthController extends Controller
 {
+
     public function __construct(protected AuthRepository $authUserRepo)
     {}
+
+    /**
+     * @OA\Post(
+     *     path="/api/auth/register",
+     *     summary="Register new user",
+     *     tags={"Auth"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name", "email", "password"},
+     *             @OA\Property(property="name", type="string", example="John"),
+     *             @OA\Property(property="email", type="string", example="john@gmail.com"),
+     *             @OA\Property(property="password", type="string", example="Password123"),
+     *             @OA\Property(property="password_confirmation", type="string", example="Password123")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="New user has been created",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Successfully"),
+     *             @OA\Property(property="data", type="object"),
+     *             @OA\Property(property="verification_url", type="string", format="url"),
+     *             @OA\Property(property="token", type="string")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Content",
+     *     @OA\JsonContent(
+     *          @OA\Property(property="message", type="string", example="Error"),
+     *     )
+     *     ),
+     * )
+     */
+
+
     public function register(RegisterRequest $request): JsonResponse
     {
 
@@ -32,6 +72,38 @@ class AuthController extends Controller
 
 
     }
+    /**
+     * @OA\Post(
+     *     path="/api/auth/login",
+     *     summary="Login exist user",
+     *     tags={"Auth"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"email", "password"},
+     *     @OA\Property(property="email", type="string", example="exist@gmail.com"),
+     *     @OA\Property(property="password", type="string", example="Password123"),
+     *      )
+     *       ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Logged succsesfully",
+     *     @OA\JsonContent(
+     *         @OA\Property(property="message", type="string", example="success")
+     *     )
+     *     ),
+     *     @OA\Response(
+     *         response=403,
+     *         description="Invalid Credentials",
+     *     @OA\JsonContent(
+     *         @OA\Property(property="message", type="string", example="Invalid email or password")
+     *     )
+     *     )
+     * )
+     *
+     *
+     */
 
     public function login(LoginRequest $request): JsonResponse
     {
@@ -51,6 +123,24 @@ class AuthController extends Controller
 
 
     }
+
+    /**
+     * @OA\Post(
+     *     path="/api/auth/logout",
+     *     summary="Logout function",
+     *     tags={"Auth"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="Logged out successfully",
+     *     @OA\JsonContent(
+     *         @OA\Property(
+     *             property="message", type="string", example="Logged out successfully")
+     *     )
+     *     )
+     * )
+     */
+
     public function logout(): JsonResponse
     {
         Auth::user()->currentAccessToken()->delete();
