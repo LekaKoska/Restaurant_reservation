6e840e959eba7e5bbbf5b1bd4465458405945ab4
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-08-05 13:52:34 +0200

    * Adding HasFactory * Making new function to return new factory

--- app/Models/TablesInfoListModel.php
+++ app/Models/TablesInfoListModel.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Database\Factories\TableInfoListFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TablesInfoListModel extends Model
 {
+    use HasFactory;
+
     protected $table = "tables_info_list";
     protected $hidden = ['id', 'created_at', 'updated_at'];
     protected $fillable = ['table_num', "location", "status"];
@@ -19,5 +23,9 @@ class TablesInfoListModel extends Model
         return $this->hasOne(TablesModel::class, "table_id", "id");
     }
 
+    public static function newFactory()
+    {
+        return TableInfoListFactory::new();
+    }
 
 }
