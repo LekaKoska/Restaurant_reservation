347d009f0a5e29ebd070698803404bd95a3bc671
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-09-10 13:38:36 +0200

    * Modifying Request for Table reservation

--- app/Http/Requests/TableReservationRequest.php
+++ app/Http/Requests/TableReservationRequest.php
@@ -22,6 +22,7 @@ class TableReservationRequest extends FormRequest
     public function rules(): array
     {
         return [
+            "user_id" => "exists:users,id",
             "guest_number" => "required|integer|min:1|max:9",
             "table_id" => "required|integer|min:1|max:23|exists:tables_info_list,id"
         ];
