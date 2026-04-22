fdc600ccbf4ee11c2b3bdeded25768dfcdbc5453
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-04-22 20:05:26 +0200

    * Fixing typo in Reservation model * Adding hasMany relation in User model to Reservations model *  Fixing factory and earsing comments

--- database/factories/TableInfoListFactory.php
+++ database/factories/TableInfoListFactory.php
@@ -10,28 +10,14 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class TableInfoListFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    protected $model = TablesInfoListModel::class;
+   
+     protected $model = TablesInfoListModel::class;
     public function definition(): array
     {
         $location = ['north', 'east', 'west', 'south'];
         return [
             'table_num' => $this->faker->numberBetween(1, 23),
             'location' => $this->faker->randomElement($location),
-            'status' => TablesInfoListModel::STATUS_AVAILABLE
         ];
     }
-
-    public function taken(): static
-    {
-        return $this->state(fn (array $attributes) =>
-        [
-            'status' => TablesInfoListModel::STATUS_TAKEN
-        ]);
-    }
-
 }
