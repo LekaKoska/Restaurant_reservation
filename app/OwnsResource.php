731ed0f86c42c8457c4a6cc785e3e0624834220e
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-04-24 10:04:21 +0200

    * Creating trait to instance check for policy

---
+++ app/OwnsResource.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use App\Models\User;
+
+trait OwnsResource
+{
+    public function isOwner(User $user, $model): bool
+    {
+        return $user->id === $model->user_id;
+    }
+}
