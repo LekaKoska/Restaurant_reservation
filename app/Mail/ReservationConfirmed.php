6c3a7efd5d24718765c9fde10b243cd053720226
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-04-24 10:05:50 +0200

    * Using model binding instead of array for mail. * Creating observer and instancing mail call

--- app/Mail/ReservationConfirmed.php
+++ app/Mail/ReservationConfirmed.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Models\Reservation;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -14,10 +15,9 @@ class ReservationConfirmed extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public function __construct(public array $data)
-    {
-        //
-    }
+    public function __construct(public Reservation $reservation)
+    {}
+
 
     public function envelope(): Envelope
     {
@@ -33,7 +33,7 @@ class ReservationConfirmed extends Mailable
             markdown: 'mail.reserved_info',
         );
     }
-    
+
     public function attachments(): array
     {
         return [];
---
+++ app/Observers/ReservationObserver.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Observers;
+
+use App\Mail\ReservationConfirmed;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Mail;
+
+class ReservationObserver
+{
+    public function created(Reservation $reservation): void
+    {
+        Mail::to($reservation->userReservations->email)->send(new ReservationConfirmed($reservation));
+    }
+
+    /**
+     * Handle the Reservation "updated" event.
+     */
+    public function updated(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "deleted" event.
+     */
+    public function deleted(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "restored" event.
+     */
+    public function restored(Reservation $reservation): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Reservation "force deleted" event.
+     */
+    public function forceDeleted(Reservation $reservation): void
+    {
+        //
+    }
+}
