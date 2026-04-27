3cebb6a18bf05b9c31f500ea44b01791fa740b3b
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-04-27 11:03:00 +0200

    * Adding Model into this service to escape undefined key error

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Reservation;
 use App\Repository\ReservationRepository;
 use Carbon\Carbon;
 
@@ -10,13 +11,14 @@ class ReservationService
     public function __construct(protected ReservationRepository $reservationRepo)
     {}
 
-    public function endTimeReservation(array $data)
+    public function endTimeReservation(array $data, Reservation $reservation)
     {
         if(array_key_exists("end_date", $data))
             {
                 return $data;
             }
-        $endDate = Carbon::parse($data["start_date"])->addHours(2);
+            $startDate = $data["start_date"] ?? $reservation->start_date;
+        $endDate = Carbon::parse($startDate)->addHours(2);
         $data["end_date"] = $endDate;
         return $data;
     }
