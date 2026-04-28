b29c96b97966b3cfdb94c8791ba0f51b3484ffc6
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-04-28 10:10:07 +0200

    * Adding 1 more ->where to ignore self. * Now user can update only part of reservation without error

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -5,13 +5,14 @@ namespace App\Services;
 use App\Models\Reservation;
 use App\Repository\ReservationRepository;
 use Carbon\Carbon;
+use Illuminate\Http\Response;
 
 class ReservationService
 {
     public function __construct(protected ReservationRepository $reservationRepo)
     {}
 
-    public function endTimeReservation(array $data, Reservation $reservation)
+    public function endTimeReservation(array $data, ?Reservation $reservation = null)
     {
         if(array_key_exists("end_date", $data))
             {
@@ -21,5 +22,22 @@ class ReservationService
         $endDate = Carbon::parse($startDate)->addHours(2);
         $data["end_date"] = $endDate;
         return $data;
+    }
+
+    public function takenTable($data, $reservationId)
+    {
+         $takenTable = Reservation::where("table_id", $data["table_id"])->where(function($query) use ($data, $reservationId)
+        {
+            $query->where("end_date", ">=", $data["start_date"]);
+            $query->where("start_date", "<=", $data["end_date"]);
+            $query->where("id", "!=", $reservationId);
+        })->exists();
+
+        if($takenTable)
+            {
+                throw new \Exception(message: "Table is taken in this period!", code: Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+        return $takenTable;
     }
 }
