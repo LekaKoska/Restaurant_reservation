8fa7dfbff7c93c7f699573dea68a141b8f0c307e
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-04-28 14:46:06 +0200

    * Instance overlap reservation into Services

--- app/Repository/ReservationRepository.php
+++ app/Repository/ReservationRepository.php
@@ -2,42 +2,21 @@
 namespace App\Repository;
 
 use App\Models\Reservation;
-use App\Services\ResponseServices;
-use Symfony\Component\HttpFoundation\Response;
+
 
 class ReservationRepository
 {
-
-
     public function  __construct(protected Reservation $reservationModel)
     {}
 
-
-    public function findUserReservation($reservation)
+    public function findUserReservation($reservation): Reservation
     {
         return Reservation::firstWhere("table_id", $reservation->id);
     }
 
-    public function creatingReservation(array $data)
+    public function creatingReservation(array $data): Reservation
     {
-        $takenTable = Reservation::where("table_id", $data["table_id"])->where(function($query) use ($data)
-        {
-            $query->where("end_date", ">=", $data["start_date"]);
-            $query->where("start_date", "<=", $data["end_date"]);
-        })->exists();
-
-        if($takenTable)
-            {
-                throw new \Exception(message: "Table is taken in this period!", code: Response::HTTP_UNPROCESSABLE_ENTITY);
-            }
-
             return Reservation::create($data);
     }
-
-     public function allUserReservations($user)
-    {
-        return $this->reservationModel->with("userReservations")->where("user_id", $user->id)->get();
-    }
-
 }
 
