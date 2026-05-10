96e9dcd4b796d67fe09e4f8b61f6dcf1fbd5f02c
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-05-10 12:35:15 +0200

    * Creating Review Request

---
+++ app/Http/Requests/ReviewRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ReviewRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            "reservation_id" => "required|exists:reservations,id",
+            "rating" => "required|min:1, max:5",
+            "comment" => "nullable|string|max:255"
+        ];
+    }
+}
