bbf926c3c340d8c0b93d9aba65dca6223e853d68
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-05-10 12:42:46 +0200

    * initial commit

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,7 +9,7 @@ class Review extends Model
     const TABLE = "reviews";
     protected $table = self::TABLE;
     protected $fillable = [
-        'reservation_id', 'user_id', 'voting',
+        'reservation_id', 'user_id', 'rating',
         'comment'
     ];
 
