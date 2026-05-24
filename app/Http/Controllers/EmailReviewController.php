d6959e0b7a9183331b7e95f26cc2eeac34eecd38
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-05-24 13:08:07 +0200

    * Implementing logic into store method for new review

--- app/Http/Controllers/EmailReviewController.php
+++ app/Http/Controllers/EmailReviewController.php
@@ -2,17 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Reservation;
+use App\Models\Review;
+use App\Services\ResponseServices;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use PHPUnit\Framework\Exception;
 
 class EmailReviewController extends Controller
 {
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        dd($request->reservation_id);
+        $reservation = Reservation::findOrFail($request->reservation_id);
+        $review = Review::firstWhere("reservation_id", $reservation->id);
+        try {
+            if($review)
+            {
+                throw new Exception(message: "Review already exists!");
+            }
+            $newReview = Review::create([
+                "reservation_id" => $request->reservation_id,
+                "user_id" => $reservation->user_id,
+                "rating" => $request->rating
+            ]);
+            return ResponseServices::successResponse(data: $newReview);
+        } catch (\Exception $e)
+        {
+            return ResponseServices::errorResponse(message: $e->getMessage());
+        }
     }
 
     public function update()
     {
-
+       //
     }
 }
