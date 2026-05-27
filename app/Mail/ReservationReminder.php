924faf01e7fe683ea7b1306aee980dc52a64227c
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-05-27 16:52:26 +0200

    * Creating new Mail class for Reservation reminder. * Now user gets mail 15 minutes before reservation.

---
+++ app/Mail/ReservationReminder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Reservation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class ReservationReminder extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(Reservation $reservation)
+    {}
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address(address: "restaurant@leka.com"),
+            subject: 'Reservation Reminder',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.reservation_reminder',
+        );
+    }
+    public function attachments(): array
+    {
+        return [];
+    }
+}
