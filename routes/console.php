37c220b34dfa91452160ce18bfa3abea883c343d
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-05-27 17:27:50 +0200

    * Scheduling command to work every 15 minutes.

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,4 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command("app:expired-reservations")->everyFiveMinutes();
+Schedule::command("app:send-reminder")->everyFifteenMinutes();
